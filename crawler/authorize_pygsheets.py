b0361a5e4d4eb55d9063fb4f0b9b211b25f36844
franciscomvargas <franciscomvargas24@gmail.com>
2023-08-07 19:34:17 +0100

    Authorize Google APIs Script Exception Handle

--- crawler/authorize_pygsheets.py
+++ crawler/authorize_pygsheets.py
@@ -4,4 +4,7 @@ import pygsheets
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 CREDENTIAL_DIR = os.path.join(CURRENT_PATH, "confidential")
 CREDENTIAL_PATH = os.path.join(CREDENTIAL_DIR, "client_secret.json")
-gc = pygsheets.authorize(client_secret=CREDENTIAL_PATH)
+try:
+    gc = pygsheets.authorize(client_secret=CREDENTIAL_PATH)
+except Exception as e:
+    print(f"[ WARNING ] - Error while authenticate: {e}")
