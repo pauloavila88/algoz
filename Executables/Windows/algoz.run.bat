6d2e14dae99fb4ab04acf64ebac0f2a49b7db59f
franciscomvargas <franciscomvargas24@gmail.com>
2023-08-09 23:25:18 +0100

    Run Algoz - Open Server Locally

--- Executables/Windows/algoz.run.bat
+++ Executables/Windows/algoz.run.bat
@@ -3,7 +3,7 @@ call cd %UserProfile%\Algoz
 @REM Activate Conda Virtual Environment
 call %UserProfile%\miniconda3\condabin\conda activate ./env
 @REM Run ZapCrawler
-call python cli.py ui
+call python cli.py ui --host 0.0.0.0 --port 80
 @REM Inform Crawl Finish
 call echo Algoz Finished !
 PAUSE
